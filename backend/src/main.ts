414414f27439f00a24b25801b20ed4d1c6d32d23
Takshsri <aymar131221@gmail.com>
2026-03-10 09:16:11 +0530

    adding main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
+app.enableCors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
