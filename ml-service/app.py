0a377232353bae071367e6455e8cc0ddca03a2d8
Takshsri <aymar131221@gmail.com>
2026-04-04 13:06:48 +0530

    updating cors

---
+++ ml-service/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(
+    app,
+    resources={r"/*": {"origins": [
+        "http://localhost:5173",
+        "https://renew-mate.vercel.app"
+    ]}},
+    supports_credentials=True
+)
