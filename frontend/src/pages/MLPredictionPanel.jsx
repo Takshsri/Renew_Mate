9c9154ee664978f43767ac8b8b9bb8bcf90454d6
Takshsri <aymar131221@gmail.com>
2026-04-06 09:05:33 +0530

    bgcolor

--- frontend/src/pages/MLPredictionPanel.jsx
+++ frontend/src/pages/MLPredictionPanel.jsx
@@ -64,7 +64,7 @@ export default function MLPredictionPanel() {
     placeholder="Service Name"
     value={formData.serviceName}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10"
+    className="p-3 rounded-xl bg-white/100 border border-white/10"
   />
 
   {/* CATEGORY */}
@@ -72,7 +72,7 @@ export default function MLPredictionPanel() {
     name="category"
     value={formData.category}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10 text-black"
+    className="p-3 rounded-xl bg-white/100 border border-white/10 text-black"
   >
     <option value="">Select Category</option>
     <option value="Entertainment">Entertainment</option>
@@ -86,7 +86,7 @@ export default function MLPredictionPanel() {
     name="billingCycle"
     value={formData.billingCycle}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10 text-black"
+    className="p-3 rounded-xl bg-white/100 border border-white/10 text-black"
   >
     <option value="MONTHLY">MONTHLY</option>
     <option value="WEEKLY">WEEKLY</option>
@@ -98,7 +98,7 @@ export default function MLPredictionPanel() {
     name="paymentMethod"
     value={formData.paymentMethod}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10 text-black"
+    className="p-3 rounded-xl bg-white/100 border border-white/10 text-black"
   >
     <option value="credit_card">Credit Card</option>
     <option value="upi">UPI</option>
@@ -109,7 +109,7 @@ export default function MLPredictionPanel() {
     name="status"
     value={formData.status}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10 text-black"
+    className="p-3 rounded-xl bg-white/100 border border-white/10 text-black"
   >
     <option value="ACTIVE">ACTIVE</option>
     <option value="CANCELLED">CANCELLED</option>
@@ -121,7 +121,7 @@ export default function MLPredictionPanel() {
     placeholder="Monthly Spending"
     value={formData.monthly_spending}
     onChange={handleChange}
-    className="p-3 rounded-xl bg-white/10 border border-white/10"
+    className="p-3 rounded-xl bg-white/100 border border-white/10"
   />
 
   <input
